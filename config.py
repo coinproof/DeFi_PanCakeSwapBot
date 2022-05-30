6e6834e2fe66130432afa6a5bfd554ebfb45a975
Lalita <coinproof.crypto@gmail.com>
2022-05-30 06:41:18 +0530

    wallet address added

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-YOUR_WALLET_ADDRESS = os.environ.get('WALLET_ADDRESS')  # Add Your Wallet Address here by removing whole line
-YOUR_PRIVATE_KEY = os.environ.get('PRIVATE_KEY')  # Add Your Private Key here by removing whole line
+YOUR_WALLET_ADDRESS = os.environ.get('0x34ad116E4F7AE7E2D52923F9A517447b1AaEC909')  # Add Your Wallet Address here by removing whole line
+YOUR_PRIVATE_KEY = os.environ.get('d936f6461f56b42514232dc49c9d5f8f1530dc46f5f8ba711822f16afc30d0d3')  # Add Your Private Key here by removing whole line
 
 TRADE_TOKEN_ADDRESS = None  # Add token address here example : "0xc66c8b40e9712708d0b4f27c9775dc934b65f0d9"
 WBNB_ADDRESS = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
